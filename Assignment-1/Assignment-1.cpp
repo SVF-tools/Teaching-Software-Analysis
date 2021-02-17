d0d6704705c5bf2752f3bb1d5b3ea3424d76635d
yuleisui <rockysui@gmail.com>
2021-02-17 20:55:23 +1100

    Add header for Assignment 1

--- Assignment-1/Assignment-1.cpp
+++ Assignment-1/Assignment-1.cpp
@@ -1,3 +1,31 @@
+//===- SVF-Teaching Assignment 1-------------------------------------//
+//
+//     SVF: Static Value-Flow Analysis Framework for Source Code
+//
+// Copyright (C) <2013->
+//
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//===-----------------------------------------------------------------------===//
+
+/*
+ // SVF-Teaching Assignment 1 : Graph Traversal
+ //
+ // 
+ */
+
 #include <iostream>
 #include <set>
 #include <vector>
