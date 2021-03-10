ff6d1df1e05c4d01de2b739e41a38bb9d9170aa6
guanqin-123 <charles.guanqin@gmail.com>
2021-03-10 11:14:00 +1100

    one press to generate

---
+++ CodeGraph/compile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ -n $1 ]; then
+    cmake ..
+    make
+    clang -S -c -Xclang -disable-O0-optnone -fno-discard-value-names -emit-llvm $1 -o $1.ll
+    opt -S -mem2reg $1.ll -o $1.ll
+    ../bin/CodeGraph $1.ll
+else
+echo "please give the .ll file"
+fi
