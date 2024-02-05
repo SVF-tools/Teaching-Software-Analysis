5a7de05331f53d58759e8e3c58fd975ae05681d0
jumormt <jumormt@163.com>
2024-02-05 14:40:20 +1100

    sync with SVF

--- env.sh
+++ env.sh
@@ -5,7 +5,7 @@ sysOS=`uname -s`
 MajorLLVMVer=16
 LLVMVer=${MajorLLVMVer}.0.0
 
-LLVMHome="LLVM-${LLVMVer}.obj"
+LLVMHome="llvm-${LLVMVer}.obj"
 Z3Home="z3.obj"
 install_path=`npm root`
 
@@ -18,16 +18,20 @@ then
 fi
 
 Z3_DIR=$install_path/$Z3Home
+SVF_DIR=$install_path/SVF
 
 export LLVM_DIR=$LLVM_DIR
 export Z3_DIR=$Z3_DIR
+export SVF_DIR=$SVF_DIR
+export PATH=$SVF_DIR/Release-build/bin:$PATH
 export PATH=$LLVM_DIR/bin:$PATH
 export PATH=$PROJECTHOME/bin:$PATH
 
 echo "export LLVM_DIR=$LLVM_DIR" >> ~/.bashrc
 echo "export Z3_DIR=$Z3_DIR" >> ~/.bashrc
-echo "export PATH=$LLVM_DIR/bin:$PROJECTHOME/bin:$PATH" >> ~/.bashrc
-export SVF_DIR=$install_path/SVF/
+echo "export SVF_DIR=$SVF_DIR" >> ~/.bashrc
+echo "export PATH=$SVF_DIR/Release-build/bin:$LLVM_DIR/bin:$PROJECTHOME/bin:$PATH" >> ~/.bashrc
+
 
 echo "LLVM_DIR="$LLVM_DIR
 echo "SVF_DIR="$SVF_DIR
