e2967b0558dbb6b51d85f872337bcf6cb905bde4
jumormt <jumormt@163.com>
2024-02-05 15:16:12 +1100

    sync with SVF

--- env.sh
+++ env.sh
@@ -11,18 +11,14 @@ install_path=`npm root`
 
 if [[ $sysOS == "Darwin" ]]
 then
-    LLVM_DIR="$(brew --prefix llvm@${MajorLLVMVer})"
+    export LLVM_DIR="$(brew --prefix llvm@${MajorLLVMVer})"
 elif [[ $sysOS == "Linux" ]]
 then
-    LLVM_DIR=$install_path/$LLVMHome
+    export LLVM_DIR=$install_path/$LLVMHome
 fi
 
-Z3_DIR=$install_path/$Z3Home
-SVF_DIR=$install_path/SVF
-
-export LLVM_DIR=$LLVM_DIR
-export Z3_DIR=$Z3_DIR
-export SVF_DIR=$SVF_DIR
+export Z3_DIR=$install_path/$Z3Home
+export SVF_DIR=$install_path/SVF
 export PATH=$SVF_DIR/Release-build/bin:$PATH
 export PATH=$LLVM_DIR/bin:$PATH
 export PATH=$PROJECTHOME/bin:$PATH
